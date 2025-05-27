9361b31f9980d45a89b827f1c853c0bdd612dee0
Voigt, Tim <tvoigt2000@gmail.com>
2025-05-27 13:54:03 +0200

    updated the csv input for invest

--- Webserver/WWSystem/webapplication/views.py
+++ Webserver/WWSystem/webapplication/views.py
@@ -1199,24 +1199,28 @@ def some_view(request):
     jahr = 2026
     ous = Ou.objects.values_list('ou_id', 'ou').distinct()
     path = "/media/daten/Invest/"
-    with open(f'{path}Monitor_27_2026.csv', 'r') as file:
+    with open(f'{path}Investplaung_2026_Desktop.csv', 'r') as file:
         reader = csv.reader(file, delimiter=";")
+        next(reader)
         for row in reader:
             ou = str(row[0]).replace("OU0", "").replace("OU", "")
             menge = row[1]
+            if not menge:
+                menge = 0
             typ = row[2]
             preis = str(row[3]).replace(" €", "").replace(".", "")
             modell = row[4]
-            for item in ous:
-                if str(item[1]).replace("(", "").replace(",)", "") == ou:
-                    ou_id = Ou.objects.get(ou_id=str(item[0]).replace("(", "").replace(",)", ""))
-                    # Detail_Investmittelplan_Soll.objects.create(ou_id=ou_id, jahr=jahr, typ=typ, modell=modell, menge=menge, preis_pro_stück=preis)
-                    # alt_gesamt = Invest.objects.values_list('investmittel_gesamt').filter(jahr=jahr).filter(ou_id=str(item[0]).replace("(", "").replace(",)", "")).filter(typ="Planung")
-                    # alt_gesamt = float(str(alt_gesamt[0]).replace("(Decimal('", "").replace("'),)", ""))
-                    # kosten = int(menge) * int(preis)
-                    # neu_gesamt = alt_gesamt + kosten
-                    # invest_id = Invest.objects.values_list('id').filter(ou_id=ou_id).filter(jahr=jahr).filter(typ="Planung")
-                    # Invest.objects.update_or_create(id=str(invest_id[0]).replace("(", "").replace(",)", ""), defaults={'investmittel_gesamt': neu_gesamt})
+            if int(menge) > 0:
+                for item in ous:
+                    if str(item[1]).replace("(", "").replace(",)", "") == ou:
+                        ou_id = Ou.objects.get(ou_id=str(item[0]).replace("(", "").replace(",)", ""))
+                        Detail_Investmittelplan_Soll.objects.create(ou_id=ou_id, jahr=jahr, typ=typ, modell=modell, menge=menge, preis_pro_stück=preis)
+                        alt_gesamt = Invest.objects.values_list('investmittel_gesamt').filter(jahr=jahr).filter(ou_id=str(item[0]).replace("(", "").replace(",)", "")).filter(typ="Planung")
+                        alt_gesamt = float(str(alt_gesamt[0]).replace("(Decimal('", "").replace("'),)", ""))
+                        kosten = int(menge) * int(preis)
+                        neu_gesamt = alt_gesamt + kosten
+                        invest_id = Invest.objects.values_list('id').filter(ou_id=ou_id).filter(jahr=jahr).filter(typ="Planung")
+                        Invest.objects.update_or_create(id=str(invest_id[0]).replace("(", "").replace(",)", ""), defaults={'investmittel_gesamt': neu_gesamt})
         
     return render(request, "webapplication/csv.html")
 
