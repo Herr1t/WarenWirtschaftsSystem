8954c2d6ac5105ee6d1b1870c7a633efe7dc966d
Voigt, Tim <tvoigt2000@gmail.com>
2025-08-21 13:41:46 +0200

    .

--- Webserver/WWSystem/webapplication/views.py
+++ Webserver/WWSystem/webapplication/views.py
@@ -1360,21 +1360,21 @@ def detail_profile_lager_ohne(request, user_id, bestell_nr):
         })
 
 def some_view(request):
-    data = Invest.objects.values_list('ou_id', 'team', 'bereich').filter(jahr=2024).filter(typ="Aktiv")
-    for item in data:
-        ou_id = str(item[0]).strip("(),")
-        ou = Ou.objects.get(pk=ou_id)
-        team = str(item[1]).strip("(),")
-        bereich = str(item[2]).strip("(),")
-        Invest.objects.create(
-            ou=ou,
-            investmittel_verausgabt=0,
-            investmittel_übrig=0,
-            investmittel_gesamt=0,
-            team=team,
-            bereich=bereich,
-            jahr=2024,
-            typ="Planung")
+    # data = Invest.objects.values_list('ou_id', 'team', 'bereich').filter(jahr=2024).filter(typ="Aktiv")
+    # for item in data:
+    #     ou_id = str(item[0]).strip("(),")
+    #     ou = Ou.objects.get(pk=ou_id)
+    #     team = str(item[1]).strip("(),")
+    #     bereich = str(item[2]).strip("(),")
+    #     Invest.objects.create(
+    #         ou=ou,
+    #         investmittel_verausgabt=0,
+    #         investmittel_übrig=0,
+    #         investmittel_gesamt=0,
+    #         team=team,
+    #         bereich=bereich,
+    #         jahr=2024,
+    #         typ="Planung")
     # ou = Ou.objects.values_list("ou")
     # preis = 0.00
     # anfang = 0.00
